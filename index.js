32d33d07cb7deba8d304f9227d6e1211ca316bb4
judy-chelangat <judysigilai75@gmail.com>
2023-06-08 17:12:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names= ["Judy", "peter", "Joseph"];
+
+const event = "birthday";
+
+
+ function writeCards( names, event ) {
+  let personalCard = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCard.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return personalCard
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
